263aeadf802001563a4e9feec5b2065a6bdc9fea
Ewooral <boahen.cos@gmail.com>
2022-01-05 23:20:25 +0000

    API's endpoint to fetch current server time functionality is done

--- README.md
+++ README.md
@@ -6,3 +6,7 @@
 yarn add express yup config cors express mongoose pino pino-pretty dayjs bcrypt jsonwebtoken lodash nanoid - optional.
 * install development dependencies
 yarn add @types/body-parser @types/config @types/cors @types/express @types/node @types/yup @types/pino @types/mongoose @types/bcrypt @types/jsonwebtoken @types/lodash @types/nanoid ts-node typescript-D 
+
+# To run the Server:
+* first download all depencies in the package.json file.
+* type: "npm run dev"
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,23 @@
-const greeting: string = 'Hello, world!';
+import express, { Application , Request, response, Response } from 'express';
 
-const numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8]
+const app: Application = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static('public'));
+
+app.get("/", (request, response) => {
+    response.sendFile(__dirname + '/views/index.html');
+});
+
+
+app.listen(PORT, () => {
+    console.log('Game Api listening on port 3000!');
+});
+
+let responseObject: any = {};
+
+
+app.get('/api/timestamp',  (Request: any, Response: any): any =>{
+    responseObject['Current UTC'] = new Date().toUTCString()
+    Response.json(responseObject)
+});
